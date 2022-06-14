4efc256bc2a2d52ff2b63db8d6c260ca6895712f
GenesisL <353339279@qq.com>
2022-06-14 17:43:59 +0800

    First Commit

---
+++ LLUtils/LLUtils.h
@@ -0,0 +1,18 @@
+//
+//  LLUtils.h
+//  LLUtils
+//
+//  Created by 林开宇 on 2022/6/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LLUtils.
+FOUNDATION_EXPORT double LLUtilsVersionNumber;
+
+//! Project version string for LLUtils.
+FOUNDATION_EXPORT const unsigned char LLUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LLUtils/PublicHeader.h>
+
+
